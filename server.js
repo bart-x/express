2c30156b3df158592aa76e6b962c0845a4bb897e
Bartek K <kontener00@gmail.com>
2020-10-19 18:57:49 +0200

    init project files

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>My first server!</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>About</h1>');
+  });
+  
+  app.get('/contact', (req, res) => {
+    res.send('<h1>Contact</h1>');
+  });
+
+  app.get('/info', (req, res) => {
+    res.send('<h1>Info</h1>');
+  });
+
+  app.get('/history', (req, res) => {
+    res.send('<h1>History</h1>');
+  });
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
+
+
+// routing in React without using Express
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+
+//   if(req.url === '/' && req.method === 'GET') {
+//     res.write('<h1>My first server!</h1>');
+//     res.end();
+//   }
+//   else if(req.url === 'about/' && req.method === 'GET') {
+//     res.write('<h1>About</h1>');
+//     res.end();
+//   }
+
+//   // ... then similar other routes
+
+// })
+
+// server.listen(8000, (err) => {
+//   if (err) {
+//     return console.log('something bad happened', err);
+//   }
+
+//   console.log(`server is listening on ${8000}`);
+// });
