59a5e962979f2565563115f8d6cd3258c1a3d444
Bartek K <kontener00@gmail.com>
2020-10-19 21:44:00 +0200

    add project files

--- server.js
+++ server.js
@@ -1,54 +1,35 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.show = (name) => {
+        res.sendFile(path.join(__dirname, `/views/${name}`));
+    };
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.send('<h1>My first server!</h1>');
+    res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-    res.send('<h1>About</h1>');
-  });
-  
-  app.get('/contact', (req, res) => {
-    res.send('<h1>Contact</h1>');
-  });
+    res.show('about.html');
+});
 
-  app.get('/info', (req, res) => {
-    res.send('<h1>Info</h1>');
-  });
+app.get('/contact', (req, res) => {
+    res.show('contact.html');
+});
 
-  app.get('/history', (req, res) => {
-    res.send('<h1>History</h1>');
-  });
+app.get('/info', (req, res) => {
+    res.show('info.html');
+});
+
+app.get('/history', (req, res) => {
+    res.show('history.html');
+});
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
 });
-
-
-// routing in React without using Express
-// const http = require('http');
-
-// const server = http.createServer((req, res) => {
-
-//   if(req.url === '/' && req.method === 'GET') {
-//     res.write('<h1>My first server!</h1>');
-//     res.end();
-//   }
-//   else if(req.url === 'about/' && req.method === 'GET') {
-//     res.write('<h1>About</h1>');
-//     res.end();
-//   }
-
-//   // ... then similar other routes
-
-// })
-
-// server.listen(8000, (err) => {
-//   if (err) {
-//     return console.log('something bad happened', err);
-//   }
-
-//   console.log(`server is listening on ${8000}`);
-// });
